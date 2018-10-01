9324db37f9b7e50d5e67a13c46d4025c3ea38e46
karimayman <karimayman95@gmail.com>
2018-10-01 21:24:11 +0200

    added custom routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -32,4 +32,11 @@ end
 def show
   @portfolio_item = Portfolio.find(params[:id])
 end
+def destroy
+  @portfolio_item = Portfolio.find(params[:id])
+  @portfolio_item.destroy
+  respond_to do |format|
+    format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+  end
+end
 end
