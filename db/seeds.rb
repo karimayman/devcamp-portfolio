005f4c10ac36def423effa09279af18209c31306
karimayman <karimayman95@gmail.com>
2018-10-02 22:33:24 +0200

    finished alot of stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |what|
-  Blog.create!(title: "my blog #{what} ",
-  body: "whatever i write herfe is valid bitch")
+#10.times do |what|
+#  Blog.create!(title: "my blog #{what} ",
+#  body: "whatever i write herfe is valid bitch")
 
-end
-puts"10 blogs created"
-5.times do |skills|
-  Skill.create!( title: "rails #{skills}",
-  percent_utilized: "15")
-end
-puts "5 skills created"
-9.times do |port|
-  Portfolio.create!(title: "portfolios #{port}",
-  subtitle: "my greatest service",
-body:"whatever  i write here is vakid again",
-main_image: "https://via.placeholder.com/600x400",
-thumb_image: "https://via.placeholder.com/350x250")
-end
-puts"portfolios successfuly created duh!"
+#end
+#puts"10 blogs created"
+##5.times do |skills|
+  #Skill.create!( title: "rails #{skills}",
+  #percent_utilized: "15")
+#end
+#puts "5 skills created"
+#9.times do |port|
+#  Portfolio.create!(title: "portfolios #{port}",
+#  subtitle: "my greatest service",
+#body:"whatever  i write here is vakid again",
+#main_image: "https://via.placeholder.com/600x400",
+#thumb_image: "https://via.placeholder.com/350x250")
+#end
+#puts"portfolios successfuly created duh!"
